27593c94de81e69419fe0b0ad6f11932be761caa
MahesaGerrardybhumi <mahesagerrardybhumi@gmail.com>
2025-09-17 00:08:38 +0700

    tugas 3

--- liverfoot_shop/settings.py
+++ liverfoot_shop/settings.py
@@ -32,6 +32,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "mahesa-gerrardybhumi-liverfootshop.pbp.cs.ui.ac.id"]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://mahesa-gerrardybhumi-liverfootshop.pbp.cs.ui.ac.id"
+]
+
 
 # Application definition
 

517b002bcbca62d525905652046f3aae19128968
MahesaGerrardybhumi <mahesagerrardybhumi@gmail.com>
2025-09-17 10:13:07 +0700

    Tugas 3

--- liverfoot_shop/settings.py
+++ liverfoot_shop/settings.py
@@ -33,7 +33,8 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "mahesa-gerrardybhumi-liverfootshop.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://mahesa-gerrardybhumi-liverfootshop.pbp.cs.ui.ac.id"
+    "https://mahesa-gerrardybhumi-liverfootshop.pbp.cs.ui.ac.id",
+    "http://mahesa-gerrardybhumi-liverfootshop.pbp.cs.ui.ac.id"
 ]
 
 
